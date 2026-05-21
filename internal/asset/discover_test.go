e0bc2f13761927d063fcd554c2561725e018ba55
Marshall Porter <meoporter@gmail.com>
2026-05-21 00:40:25 -0700

    fix tests

--- internal/asset/discover_test.go
+++ internal/asset/discover_test.go
@@ -87,8 +87,12 @@ func TestDiscoverPaths_MultipleSkip(t *testing.T) {
 	old := os.Stdin
 	os.Stdin = r
 	defer func() { os.Stdin = old }()
-	w.WriteString("0\n")
-	w.Close()
+	if _, err = w.WriteString("0\n"); err != nil {
+		t.Fatal(err)
+	}
+	if err = w.Close(); err != nil {
+		t.Fatal(err)
+	}
 
 	_, _, discoverErr := DiscoverPaths(dir, "mytool")
 	if discoverErr != ErrSkip {
